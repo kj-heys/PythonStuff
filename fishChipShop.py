46fb324f25d34db7302fd33b480cc3f11b2ac119
Kev Heys <kheys@walsallcollege.ac.uk>
2024-01-08 14:30:33 +0000

    WORK THIS TIME

---
+++ fishChipShop.py
@@ -0,0 +1,21 @@
+def food():
+    global orders 
+    orders = ['SFAC', 'LFAC', 'SAKP' , 'SAKPWC' , 'RSAC']
+    x = int(input("Please make a selection: "))
+    if(x == 1):
+        print(orders[0] , "has been chosen.")
+    if(x == 2):
+        print(orders[1] , "has been chosen. ")
+    if(x == 3):
+        print(orders[2] , "has been chosen. ")
+    if(x == 4):
+        print(orders[3] , "has been chosen. ")
+    if(x == 5):
+        print(orders[4] , "has been chosen. ")
+    else:
+        print("Invalid option made. Process closed. Good Bye!")
+
+while(True):
+    food()
+    #hello from me
+    print("hello")
