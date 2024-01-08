fcaa78af0b56107e0efe4fe5fc9a7dbbe942d758
Kev Heys <kheys@walsallcollege.ac.uk>
2024-01-08 14:49:10 +0000

    please god

---
+++ tupleexample.py
@@ -0,0 +1,18 @@
+myTuple = ("Cat", "Dog", "Penguin", "Zebra")
+print(len(myTuple))
+
+
+myTuple = ("Cat", "Dog", "Penguin", "Zebra")
+print(myTuple[1])
+
+myTuple = ("Cat", "Dog", "Penguin", "Zebra")
+print(myTuple[-1])
+
+x = ("Cat", "Dog", "Penguin", "Zebra")
+y = list(x)
+y[1] = "Cow"
+x = tuple(y)
+
+print(x)
+
+print("never ever leave your machine open")
